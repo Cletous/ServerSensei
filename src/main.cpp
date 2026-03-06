18db5affcfd9df5a90b04de83b3866c5a1832949
Cletous <ngomacletousjnr@gmail.com>
2026-03-06 10:58:31 +0200

    blank init

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+// put function declarations here:
+// e.g., #define LED_BUILTIN 2
+
+// This code run once
+void setup()
+{
+  //
+}
+
+// put your main code here, to run repeatedly:
+void loop()
+{
+  //
+}
+
+// put function definitions here:
