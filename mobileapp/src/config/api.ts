fa7ad04388d7d954e88669f5c04a29b6fcbd7245
Cletous <ngomacletousjnr@gmail.com>
2026-06-13 10:29:36 +0200

    Update api.ts

--- mobileapp/src/config/api.ts
+++ mobileapp/src/config/api.ts
@@ -1,7 +1,7 @@
 import { Platform } from "react-native";
 
 const WEB_API_BASE_URL = "http://127.0.0.1:8000";
-const MOBILE_API_BASE_URL = "http://10.128.44.124:8000";
+const MOBILE_API_BASE_URL = "http://10.131.156.124:8000";
 
 export const API_BASE_URL =
   Platform.OS === "web" ? WEB_API_BASE_URL : MOBILE_API_BASE_URL;
